9846bc995565ececf1db6ba087df948e6b04521c
MaximoRomano <102158982+MaximoRomano@users.noreply.github.com>
2022-03-29 10:40:26 -0300

    Guia 01 continued

--- Assets/script/ej 01.cs
+++ Assets/script/ej 01.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ej01 : MonoBehaviour {
+    //Realizá un programa donde se cree una variable entera llamada num1, que inicialmentevaldrá 0. Luego incrementá su valor en 2 y mostralo por pantalla.Después mostrá elresultado de multiplicarlo por sí mismo.//
+    // Use this for initialization
+        void Start ()
+        {
+        int num1 = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	    }
 	
 	// Update is called once per frame
 	void Update () {
